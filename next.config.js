9b0154fdbcec15f26623fb3e4ecf41026cde7376
giovanna-mrosa <gmarcaro@gmail.com>
2022-08-17 11:59:48 -0300

    :wrench: fix: Stopped tracking next.config.js for security

---
+++ next.config.js
@@ -0,0 +1,11 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  env: {
+    SERVICE_ID_EMAILJS: 'service_6fcwqnv',
+    TEMPLATE_ID_EMAILJS: 'template_c7g9zql',
+    PUBLIC_KEY_EMAILJS: 'I8LYQHrNPwhdNyb_S'
+  }
+}
+
+module.exports = nextConfig
