0c0678e1c45a8cd03dca40f5ba28cae0bca5969b
giovanna-mrosa <gmarcaro@gmail.com>
2022-09-19 11:07:38 -0300

    feat: synolis page

--- pages/synolis.jsx
+++ pages/synolis.jsx
@@ -1,8 +1,38 @@
-import React from 'react';
+import Image from 'next/image';
+import { useEffect, useState } from 'react'
+import Navbar from '/components/Navbar'
+import NavbarMobile from '/components/NavbarMobile'
+
+import style from '../styles/Synolis.module.scss'
 
 export default function Synolis() {
-  return (    
-      <h1>bola</h1>
-    
+  const [isMobile, setIsMobile] = useState(false)
+
+  useEffect(() => {
+    if (window.screen.width <= 1024) {
+      setIsMobile(true)
+    }
+  }, [])
+
+  return (   
+      <>
+        <header className={style.header}>
+          {isMobile ? (
+            <NavbarMobile />
+          ) : (
+            <Navbar />
+          )            
+          }   
+        </header>
+        
+        <div /*className={style.cheetah}*/>
+          <Image 
+            src="/images/cheetah.png"
+            width="560"
+            height="315"
+            alt=""          
+            />
+        </div> 
+      </>   
   )
 }
