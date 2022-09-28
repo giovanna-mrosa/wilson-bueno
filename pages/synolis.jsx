66b9ed4706bebfabb03556d4126bb60d3595145f
giovanna-mrosa <gmarcaro@gmail.com>
2022-09-28 16:49:08 -0300

    feat: synolis page content

--- pages/synolis.jsx
+++ pages/synolis.jsx
@@ -9,6 +9,16 @@ import { CaretLeft, Plus } from 'phosphor-react';
 
 export default function Synolis() {
   const [isMobile, setIsMobile] = useState(false)
+  const [openInfoTreat, setOpenInfoTreat] = useState(false)
+  const [openInfoDisease, setOpenInfoDisease] = useState(false)
+
+  function handleBoxInfoTreat() {
+    setOpenInfoTreat(!openInfoTreat)
+  }
+
+  function handleBoxInfoDisease() {
+    setOpenInfoDisease(!openInfoDisease)
+  }
 
   useEffect(() => {
     if (window.screen.width <= 1024) {
@@ -59,6 +69,7 @@ export default function Synolis() {
             />
           </div>
         </section>
+
         <section className={style.disease}>
           <div className={style.rowImage}>
             <Image
@@ -69,13 +80,71 @@ export default function Synolis() {
                 className={style.knee}
               />
               <p>Clique aqui</p>
-              <Plus size={70} className={style.plus} />
+              <Plus size={70} className={style.plus} onClick={() => handleBoxInfoDisease()} />
           </div>
           <h2>Artrose, <br />
             Osteoartrite ou <br />
             Osteoartrose
           </h2>
         </section>
+
+        {openInfoDisease === true && (
+            <section className={style.box}>
+              <div className={style.diseaseContent}>
+                <div className={style.diseaseContentText}>
+                  <p>
+                    É uma doença articular crônica caracterizada pela destruição
+                    progressiva da cartilagem nas articulações. Ela ataca o fluído
+                    sinovial saudável, causando a disfunção e a degradação do ácido
+                    hialurônico, principal componente desse fluído, alterando o peso
+                    molecular dentro da articulação, causando a desidratação da
+                    cartilagem e consequentemente a morte celular dos condrócitos,
+                    as únicas células contidas nessa cartilagem. <br /> <br />
+                    O fluído sinovial tem as funções de reduzir a fricção através
+                    da lubrificação, absorver impactos e proteger a cartilagem
+                    articular, além de fornecer oxigênio e nutrientes à cartilagem
+                    articular. Ao perder sua viscosidade, que é responsável pela
+                    lubrificação, e sua elasticidade, responsável pela proteção
+                    contra impactos, há o aumento do atrito e impacto dos ossos na
+                    cartilagem, que juntamente com a desidratação, torna a
+                    destruição progressiva e a diminuição de espessura inevitáveis.
+                    Tal processo de degeneração causa perda de mobilidade, muita
+                    dor e, consequentemente, perda na qualidade de vida. O
+                    desfecho, quando não ocorre o devido diagnóstico e tratamento,
+                    é a destruição completa da cartilagem e a necessidade de
+                    implantar-se uma prótese através de cirurgia. <br /> <br />
+                    As articulações mais acometidas pela doença são primeiramente
+                    joelhos, pelo maior impacto, seguido pelos quadris, ombros e
+                    posteriormente articulações menores, como mãos e tornozelos.
+                  </p>
+                  <div className={style.symptoms}>
+                    <p>Sintomas</p>
+                    <div className={style.more}>
+                      <Plus size={25} />
+                    </div>
+                  </div>
+                  <div className={style.factors}>
+                    <p>Fatores Atribuídos</p>
+                    <div className={style.more}>
+                      <Plus size={25}/>
+                    </div>
+                  </div>
+                </div>
+                <Image
+                src="/images/body.png"
+                width="450"
+                height="600"
+                alt=""
+                className={style.knee}
+              />
+              </div>
+              <p className={style.warning}>
+                Quando houver suspeitas e/ou apresentar alguns dos sintomas, 
+                procure imediatamente seu médico
+              </p>
+            </section>
+          )}
+
         <section className={style.treatment}>
           <h2>Tratamento</h2>
           <div className={`${style.rowImage} ${style.rowImage2}`}>
@@ -83,13 +152,42 @@ export default function Synolis() {
                 src="https://images.unsplash.com/photo-1483721310020-03333e577078?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=928&q=80"
                 width="456"
                 height="456"
-                alt="Synolis logo"
+                alt=""
                 className={style.knee}
               />
               <p>Clique aqui</p>
-              <Plus size={70} className={style.plus} />         
-          </div>          
+              <Plus size={70} className={style.plus} onClick={() => handleBoxInfoTreat()} />         
+          </div> 
         </section>
+
+        {openInfoTreat === true && (
+            <section className={style.box}>
+              <div className="treatamentContent">
+                <p>
+                  Sempre será o médico que determinará o tratamento mais adequado. 
+                  Dentro das opções propostas, a viscossuplementação com ácido 
+                  hialurônico encontra-se nos tratamentos intra-articulares. Ela 
+                  é um procedimento geralmente rápido e seguro, se feita pelo 
+                  profissional correto, tem um ótimo custo-benefício ao paciente, 
+                  e visa devolver rapidamente a qualidade de vida ao paciente e 
+                  postergar, ou mesmo eliminar a necessidade da cirurgia de 
+                  prótese. Dos profissionais que a utilizam, encontramos 
+                  ortopedistas, reumatologistas, fisiatras, médicos 
+                  intervencionistas da dor e médicos do esporte. 
+                </p>
+                <Image
+                src="/images/gestão-tratamento.svg"
+                width="556"
+                height="424"
+                alt=""
+              />
+              </div>
+              <p className={style.warning}>
+                Quando houver suspeitas e/ou apresentar alguns dos sintomas, 
+                procure imediatamente seu médico
+              </p>
+            </section>
+          )}
 
         <section className={style.synolisContent}>
           <p>
@@ -119,6 +217,15 @@ export default function Synolis() {
             <source src="/images/range.mp4#t=2.8" />
           </video>
           <p>
+            A linha possui duas apresentações: uma delas traz na embalagem uma 
+            seringa com 2 mL de volume apresentando 40 mg de ácido hialurônico e 
+            80mg de sorbitol, e a outra apresenta seringa com 4 mL de volume com 
+            80 mg de ácido hialurônico e 160 mg de sorbitol. A seringa de 2mL é 
+            destinada ao tratamento de articulações sinoviais menores, como é o 
+            caso dos ombros. A seringa de 4mL é para as articulações maiores, 
+            como joelhos e quadris. Ambas seguem o regime de dose única.            
+            <br />
+            <br />
             Após o SynolisVA ser infiltrado na cápsula articular, este assumirá 
             o papel de ser novamente o fluido sinovial jovem e saudável na 
             articulação do paciente. Assim, com a viscoelasticidade restaurada, 
@@ -126,15 +233,7 @@ export default function Synolis() {
             eliminando o atrito dos ossos, e também por um fluido elástico, que 
             protegerá a cartilagem do impacto causados, devolvendo a esta o 
             melhor ambiente possível para que ela passe a viabilizar a vida de 
-            seus condrócitos, e devolver ao paciente a melhor qualidade de vida. <br />
-            <br />
-            A linha possui duas apresentações: uma delas traz na embalagem uma 
-            seringa com 2 mL de volume apresentando 40 mg de ácido hialurônico e 
-            80mg de sorbitol, e a outra apresenta seringa com 4 mL de volume com 
-            80 mg de ácido hialurônico e 160 mg de sorbitol. A seringa de 2mL é 
-            destinada ao tratamento de articulações sinoviais menores, como é o 
-            caso dos ombros. A seringa de 4mL é para as articulações maiores, 
-            como joelhos e quadris. Ambas seguem o regime de dose única.
+            seus condrócitos, e devolver ao paciente a melhor qualidade de vida. 
           </p>
         </section>
         <p className={style.source}><span>Fonte: </span>synolis.com/pt-br/</p>
